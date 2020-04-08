855d7759ca5cfbb44d6e8abb6b1ce6ea68da6b06
darkAlert <darkangel-nwo@yandex.ru>
2020-04-08 12:25:15 +0300

    add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name='vibert',
+    version='1.0.0',
+    description='VIBE-RT: Real-time Video Inference for Human Body Pose and Shape Estimation',
+    author='mkocabas,vadim,darkAlert',
+    packages=setuptools.find_packages()
+)
